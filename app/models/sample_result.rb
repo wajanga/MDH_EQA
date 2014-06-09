da1d24efd1ab3368e4ff1404e8fda765ab25ef57
Aron Kondoro <awkondoro@gmail.com>
2014-06-09 15:35:27 +0300

    small bugfixes

--- app/models/sample_result.rb
+++ app/models/sample_result.rb
@@ -2,4 +2,11 @@ class SampleResult < ActiveRecord::Base
 
 	belongs_to :result
 
+	validates :d_result, inclusion: { in: %w(REACTIVE NON-REACTIVE), 
+		message: "%{value} is not a valid sample result" }
+	validates :u_result, inclusion: { in: %w(REACTIVE NON-REACTIVE N/A), 
+		message: "%{value} is not a valid sample result" }
+	validates :f_result, inclusion: { in: %w(POSITIVE NEGATIVE), 
+		message: "%{value} is not a valid final result" }
+
 end
--- app/models/sent_sample.rb
+++ app/models/sent_sample.rb
@@ -3,4 +3,11 @@ class SentSample < ActiveRecord::Base
 	belongs_to :facility
 	belongs_to :eqa_test
 
+	validates :d_expected_result, inclusion: { in: %w(REACTIVE NON-REACTIVE), 
+		message: "%{value} is not a valid sample result" }
+	validates :u_expected_result, inclusion: { in: %w(REACTIVE NON-REACTIVE N/A), 
+		message: "%{value} is not a valid sample result" }
+	validates :f_expected_result, inclusion: { in: %w(POSITIVE NEGATIVE), 
+		message: "%{value} is not a valid final result" }
+
 end
